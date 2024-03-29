cf889417146b236ae4c659a009666dfa1e4672df
taeho <hirooms2@hanyang.ac.kr>
2024-03-29 18:52:48 +0900

    24.03.29 llama_train.py 정리

--- llama_train.py
+++ llama_train.py
@@ -156,35 +156,9 @@ def llama_finetune(
 
         return result
 
-    def generate_and_tokenize_prompt(data_point, writeFlag=None):
-        # full_prompt = prompter.generate_prompt(
-        #     data_point["instruction"],
-        #     data_point["input"],
-        #     data_point["output"],
-        # )
+    def generate_and_tokenize_prompt(data_point):
         full_prompt = data_point['instruction']
-        # if writeFlag:
-        #     args.score_file.write("==========First Training sample==========\n")
-        #     args.score_file.write(f"{full_prompt}\n")
-
         tokenized_full_prompt = tokenize(full_prompt)
-        # if not train_on_inputs:
-        #     user_prompt = prompter.generate_prompt(
-        #         data_point["instruction"], data_point["input"]
-        #     )
-        #     tokenized_user_prompt = tokenize(
-        #         user_prompt, add_eos_token=add_eos_token
-        #     )
-        #     user_prompt_len = len(tokenized_user_prompt["input_ids"])
-        #
-        #     if add_eos_token:
-        #         user_prompt_len -= 1
-        #
-        #     tokenized_full_prompt["labels"] = [
-        #                                           -100
-        #                                       ] * user_prompt_len + tokenized_full_prompt["labels"][
-        #                                                             user_prompt_len:
-        #                                                             ]  # could be sped up, probably
         return tokenized_full_prompt
 
     quantization_config = BitsAndBytesConfig(load_in_8bit=True)  # , llm_int8_enable_fp32_cpu_offload=True)
