c4d7b60bb0cc5ce84e1840751763cd69864b3ca3
pyo9912 <pyo9912@naver.com>
2024-04-01 21:54:34 +0900

    240401 add prompt template

--- utils/parser.py
+++ utils/parser.py
@@ -11,7 +11,7 @@ def parse_args():
     parser.add_argument('--write', type=bool, default=False)
     parser.add_argument('--peft', type=str, default="lora")
     parser.add_argument('--mode', type=str, default="test")
-    parser.add_argument('--prompt', type=str, default="D2P", choices=['D2P', 'DP2R', 'UDP2I', 'pretrain', 'DP2R_new'])
+    parser.add_argument('--prompt', type=str, default="D2P", choices=['D2P', 'DP2R', 'UDP2I', 'pretrain', 'DP2R_new', 'DP2R_v3'])
     parser.add_argument('--train_know_file', type=str, default="espresso")
     parser.add_argument('--test_know_file', type=str, default="espresso")
 
