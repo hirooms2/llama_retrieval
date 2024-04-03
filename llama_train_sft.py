f9a66dfafa9e00a88100fef8ae1e381a9a681e3b
taeho <hirooms2@hanyang.ac.kr>
2024-04-03 16:17:28 +0900

    24.04.03 SFT test

--- llama_train_sft.py
+++ llama_train_sft.py
@@ -160,15 +160,15 @@ def llama_finetune_sft(
         tokenized_full_prompt = tokenize(full_prompt)
         return tokenized_full_prompt
 
-    quantization_config = BitsAndBytesConfig(load_in_8bit=True)  # , llm_int8_enable_fp32_cpu_offload=True)
-    # compute_dtype = getattr(torch, 'float16')
-    # print(compute_dtype)
-    # quantization_config = BitsAndBytesConfig(
-    #     load_in_4bit=True,
-    #     bnb_4bit_use_double_quant=False,
-    #     bnb_4bit_quant_type="nf4",
-    #     bnb_4bit_compute_dtype=compute_dtype
-    # )
+    # quantization_config = BitsAndBytesConfig(load_in_8bit=True)  # , llm_int8_enable_fp32_cpu_offload=True)
+    compute_dtype = getattr(torch, 'float16')
+    print(compute_dtype)
+    quantization_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_use_double_quant=False,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_compute_dtype=compute_dtype
+    )
 
     data = []
     for inst, lab in zip(instructions, labels):
