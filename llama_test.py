c28af4826b2cebb1778b790e174bb791a08fbef9
taeho <hirooms2@hanyang.ac.kr>
2024-04-22 12:33:25 +0900

    24.04.22 Evaluation error

--- llama_test.py
+++ llama_test.py
@@ -222,9 +222,9 @@ class LLaMaEvaluator:
                                                          'hitgen': '%.4f' % hitgen,
                                                          'hit_scores': '|'.join(['%.4f' % i for i in [hit1, hit3, hit5]]),
                                                          'bleu_scores': '|'.join(['%.4f' % i for i in [bleu1, bleu2, bleu3, bleu4]]),
-                                                         'contain': response[0] in dialog,
-                                                         'llama_hit': label in response[0],
-                                                         'espresso_hit': label in dialog}, ensure_ascii=False) + '\n')
+                                                         'contain': response[0].strip() in dialog.strip(),
+                                                         'llama_hit': label.strip() in response[0].strip(),
+                                                         'espresso_hit': label.strip() in dialog.strip()}, ensure_ascii=False) + '\n')
 
         if not self.args.write:
             self.args.log_file.write(f'\n---Accuracy results for {self.args.log_name} at epoch {epoch}---\n')
