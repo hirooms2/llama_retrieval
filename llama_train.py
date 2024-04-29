bbb096c11ac7ae47270e3f6f7e4364f38f9bf112
KIMTAEHO <hirooms2@hanyang.ac.kr>
2024-04-29 17:04:33 +0900

    240429. UDP2I

--- llama_train.py
+++ llama_train.py
@@ -125,10 +125,10 @@ def llama_finetune(
 
     world_size = int(os.environ.get("WORLD_SIZE", 1))
     print("world_size: %d" % world_size)
-    # ddp = world_size != 1
-    # if world_size != 1:
-    #     # device_map = {"": int(os.environ.get("LOCAL_RANK") or 0)}
-    #     gradient_accumulation_steps = gradient_accumulation_steps // world_size
+    ddp = world_size != 1
+    if ddp:
+        device_map = {"": int(os.environ.get("LOCAL_RANK") or 0)}
+        gradient_accumulation_steps = gradient_accumulation_steps // world_size
 
     # Check if parameter passed or if set within environ
     use_wandb = len(wandb_project) > 0 or (
