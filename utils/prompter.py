36a89933a0e7390676ec3b158c272120be38bef0
KIMTAEHO <hirooms2@hanyang.ac.kr>
2024-05-02 10:03:26 +0900

    240502: DGI2R

--- utils/prompter.py
+++ utils/prompter.py
@@ -41,6 +41,10 @@ class Prompter(object):
                 instructions.append(self.generate_prompt(instruction=data['dialog'], input=predicted_know, label=label, mode=mode))
             elif 'D2R' in self.args.prompt:
                 instructions.append(self.generate_prompt(instruction=data['dialog'], label=label, mode=mode))
+            elif 'DGI2R' in self.args.prompt:
+                num_items = 2 if mode == 'train' else 1
+                guide = f"Goal: {data['predicted_goal'][0]} | Topic: {' or '.join(data['predicted_topic'][:num_items])}"
+                instructions.append(self.generate_prompt(instruction=data['dialog'], input=guide, label=label, mode=mode))
             elif 'DP2I' == self.args.prompt:
                 instructions.append(self.generate_prompt(instruction=data['dialog'], input=predicted_know, label=label, mode=mode))
             elif 'UDP2I' == self.args.prompt:
