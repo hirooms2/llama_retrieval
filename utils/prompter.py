ddb44ec2d0a0f8ade93773e068e36f201560a550
KIMTAEHO <hirooms2@hanyang.ac.kr>
2024-05-02 21:20:30 +0900

    240502: rollback

--- utils/prompter.py
+++ utils/prompter.py
@@ -47,7 +47,7 @@ class Prompter(object):
                 instructions.append(self.generate_prompt(instruction=data['dialog'], input=guide, label=label, mode=mode))
             elif 'DG2P' in self.args.prompt:
                 # num_items = 2 if mode == 'train' else 1
-                guide = f"Goal: {data['predicted_goal'][0]} | Topic: {' or '.join(data['predicted_topic'][:2])}"
+                guide = f"Goal: {data['predicted_goal'][0]} | Topic: {' or '.join(data['predicted_topic'][:1])}"
                 # instructions.append(self.generate_prompt(instruction=data['dialog'],  label=label, mode=mode))
                 instructions.append(self.generate_prompt(instruction=data['dialog'], input=predicted_know, input2=guide, label=label, mode=mode))
             elif 'DP2I' == self.args.prompt:
