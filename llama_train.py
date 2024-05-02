ae049013621402ed12ad4439ae432315f2c00afd
KIMTAEHO <hirooms2@hanyang.ac.kr>
2024-05-02 16:16:03 +0900

    240502: rollback

--- llama_train.py
+++ llama_train.py
@@ -324,21 +324,15 @@ def llama_finetune(
             label = f"{relevant_idx + 1}. {target_knowledge}"
 
             if 'D2P' in args.prompt:
-                full_prompt = self.prompter.generate_prompt(instruction=data['dialog'], input=predicted_know,
-                                                            label=label, mode='train')
+                full_prompt = self.prompter.generate_prompt(instruction=data['dialog'], input=predicted_know, label=label, mode='train')
             elif 'DI2P' in args.prompt:
-                full_prompt = self.prompter.generate_prompt(instruction=data['dialog'], input=predicted_know,
-                                                            input2=data['topic'], label=label, mode='train')
+                full_prompt = self.prompter.generate_prompt(instruction=data['dialog'], input=predicted_know, input2=data['topic'], label=label, mode='train')
             elif 'DP2I' == args.prompt:
-                full_prompt = self.prompter.generate_prompt(instruction=data['dialog'],
-                                                            input=predicted_know,
-                                                            label=label,
-                                                            mode='train')
+                full_prompt = self.prompter.generate_prompt(instruction=data['dialog'], input=predicted_know, label=label, mode='train')
             elif 'DG2P' == args.prompt:
                 # num_items = 2 if mode == 'train' else 1
                 guide = f"Goal: {data['predicted_goal'][0]} | Topic: {' or '.join(data['predicted_topic'][:2])}"
-                # instructions.append(self.generate_prompt(instruction=data['dialog'],  label=label, mode=mode))
-                instructions.append(self.generate_prompt(instruction=data['dialog'], input=predicted_know, input2=guide, label=label, mode='train'))
+                full_prompt = self.prompter.generate_prompt(instruction=data['dialog'], input=predicted_know, input2=guide, label=label, mode='train')
 
             tokenized_full_prompt = tokenize(full_prompt)
             # item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
