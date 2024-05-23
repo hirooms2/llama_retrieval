3c0354f8b8dceda6a1f72201429352adf538eaf4
KIMTAEHO <hirooms2@hanyang.ac.kr>
2024-05-23 19:19:41 +0900

    240520_combined pseudo

--- utils/prompter.py
+++ utils/prompter.py
@@ -28,12 +28,23 @@ class Prompter(object):
         instructions = []
 
         for data, label in zip(dataset_input, dataset_output):
+            predicted_topic_list = deepcopy(data['predicted_topic'][:self.args.topk_topic])
+            if self.args.topic_conf != 1:
+                predicted_topic_conf_list = deepcopy(data['predicted_topic_confidence'][:self.args.topk_topic])
+                cum_prob = 0
+                candidate_topic_entities = []
+                for p_topic, p_conf in zip(predicted_topic_list, predicted_topic_conf_list):
+                    if cum_prob < self.args.topic_conf:
+                        candidate_topic_entities.append(p_topic)
+                        cum_prob += p_conf
+                predicted_topic_list = deepcopy(candidate_topic_entities)
+
             if 'predicted_know' in data:
                 if self.args.combined:
                     partition = int(len(data['predicted_know']) / 2)
                     n_partition_negative = int(self.args.n_docs / 2)
 
-                    top1_negative_candidates = data['predicted_know'][0:n_partition_negative]
+                    top1_negative_candidates = data['predicted_know'][:n_partition_negative]
                     top2_negative_candidates = data['predicted_know'][partition:partition + n_partition_negative]
 
                     top1_negative_candidates = [i for i in top1_negative_candidates if data['predicted_topic'][0].lower().strip() in i.lower().strip()]
@@ -42,7 +53,10 @@ class Prompter(object):
                     top1_negative_candidates = [f"{data['predicted_topic'][0]}|{i}" for i in top1_negative_candidates]
                     top2_negative_candidates = [f"{data['predicted_topic'][1]}|{i}" for i in top2_negative_candidates]
 
-                    predicted_know = top1_negative_candidates + top2_negative_candidates
+                    top_negative_candidates = [top1_negative_candidates, top2_negative_candidates]
+                    predicted_know = []
+                    for i in range(len(predicted_topic_list)):
+                        predicted_know += top_negative_candidates[i]
                     if len(predicted_know) == 0:
                         predicted_know = data['predicted_know'][:self.args.n_docs]
                 else:
@@ -76,32 +90,20 @@ class Prompter(object):
                 #             cum_prob += p_conf
                 #     guide = f"Goal: {data['predicted_goal'][0]} | Topic: {' or '.join(candidate_topic_entities)}"
                 # else:
-                guide = f"Goal: {data['predicted_goal'][0]} | Topic: {' or '.join(data['predicted_topic'][:1])}"
+                guide = f"Goal: {data['predicted_goal'][0]} | Topic: {' or '.join(predicted_topic_list)}"
 
                 # instructions.append(self.generate_prompt(instruction=data['dialog'],  label=label, mode=mode))
                 instructions.append(self.generate_prompt(instruction=data['dialog'], input=predicted_know, input2=guide, label=label, mode=mode))
             elif 'UDP2GP' == self.args.prompt:
                 instructions.append(self.generate_prompt(instruction=data['dialog'], input=predicted_know, input2=data['user_profile'], label=label, mode=mode))
             elif 'DGIP2GIP' == self.args.prompt:
-                instructions.append(self.generate_prompt(instruction=data['dialog'], input=data['predicted_goal'][0], input2=", ".join(data['predicted_topic'][:self.args.topk_topic]), input3=predicted_know, label=label, mode=mode))
+                instructions.append(self.generate_prompt(instruction=data['dialog'], input=data['predicted_goal'][0], input2=", ".join(predicted_topic_list), input3=predicted_know, label=label, mode=mode))
             elif 'UDGIP2P' == self.args.prompt or 'UDGIP2GIP' == self.args.prompt:
-                predicted_topic_list = deepcopy(data['predicted_topic'][:self.args.topk_topic])
-                if self.args.topic_conf != 1:
-                    predicted_topic_conf_list = deepcopy(data['predicted_topic_confidence'][:self.args.topk_topic])
-                    cum_prob = 0
-                    candidate_topic_entities = []
-                    for p_topic, p_conf in zip(predicted_topic_list, predicted_topic_conf_list):
-                        if cum_prob < self.args.topic_conf:
-                            candidate_topic_entities.append(p_topic)
-                            cum_prob += p_conf
-                    predicted_topic_list = deepcopy(candidate_topic_entities)
                 instructions.append(self.generate_prompt(instruction=data['dialog'], input=data['predicted_goal'][0], input2=", ".join(predicted_topic_list), input3=predicted_know,
                                                          input4=data['user_profile'], label=label, mode=mode))
             elif 'UDGI2GI' == self.args.prompt:
                 if mode == 'train':
                     label = f"Goal: {data['goal']}\nTopic: {data['topic']}"
-                predicted_topic_list = deepcopy(data['predicted_topic'][:self.args.topk_topic])
-                # random.shuffle(predicted_topic_list)
                 instructions.append(self.generate_prompt(instruction=data['dialog'], input=data['predicted_goal'][0], input2=", ".join(predicted_topic_list), input3=data['user_profile'], label=label, mode=mode))
             elif 'DP2GP' == self.args.prompt:
                 instructions.append(self.generate_prompt(instruction=data['dialog'], input=predicted_know, label=label, mode=mode))
