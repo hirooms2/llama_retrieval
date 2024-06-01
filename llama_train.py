0f56d4159665d587175764fc21ea8cbc4c713139
KIMTAEHO <hirooms2@hanyang.ac.kr>
2024-06-01 14:35:25 +0900

    240601. DGIP2I

--- llama_train.py
+++ llama_train.py
@@ -319,7 +319,7 @@ def llama_finetune(
                 label = f"{guide}\nPassage:{label}"
                 full_prompt = self.prompter.generate_prompt(instruction=data['dialog'], input=predicted_know, label=label, mode=mode)
             elif 'DGIP2I' in args.prompt:
-                topic_idx = 1 if data['predicted_topic'][0] == data['topic'] else 2
+                topic_idx = 1 if predicted_topic[0] == data['topic'] else 2
                 label = f"Topic {topic_idx}. {data['topic']}"
                 candidate_topics = '\n'.join([f"Topic {idx + 1}. {t}" for idx, t in enumerate(predicted_topic)])
                 full_prompt = self.prompter.generate_prompt(instruction=data['dialog'], input=predicted_goal, input2=candidate_topics, input3=predicted_know, label=label, mode=mode)
