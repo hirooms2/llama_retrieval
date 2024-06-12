c44f47ae0688d7f36b13f252a59f286ee6c45f7a
KIMTAEHO <hirooms2@hanyang.ac.kr>
2024-06-12 10:31:34 +0900

    240611. long cot

--- llama_train.py
+++ llama_train.py
@@ -354,7 +354,7 @@ def llama_finetune(
             elif 'UDGIP2I_cot' == args.prompt:
                 # label = f"{data['topic']}"
                 topic_idx = 1 if predicted_topic[0] == data['topic'] else 2
-                rationale = data['topic_cot'].split('Therefore')[0].strip()
+                rationale = data['topic_cot_long'].split('Therefore')[0].strip()
                 label = f"{rationale} Therefore, the most suitable topic is \"{data['topic']}\""
                 candidate_topics = '\n'.join([f"Topic {idx + 1}. {t}" for idx, t in enumerate(predicted_topic)])
                 full_prompt = self.prompter.generate_prompt(instruction=data['dialog'], input=predicted_goal,
