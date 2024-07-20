2bdef908aeae319bcebb83f2a04e0c96b2033f6e
pyo9912 <pyo9912@naver.com>
2024-07-20 14:10:54 +0900

    240720. inspired 추가

--- utils/utils.py
+++ utils/utils.py
@@ -48,6 +48,14 @@ def augment_dataset(args, know_dataset, labels, topics):
                     new_know_dataset.append(i)
                     new_labels.append(j)
                     new_topics.append(k)
+        elif args.inspired and i['topic'] not in i['predicted_topic'][:args.topk_topic]:
+            if args.positive == 'gpt_selection':
+                if i['gpt_selection'] != '':
+                    i['predicted_topic'] = [i['topic']] + i['predicted_topic']
+                    i['predicted_know'] = [i['candidate_knowledges_gpt']] + i['predicted_know']
+                    new_know_dataset.append(i)
+                    new_labels.append(j)
+                    new_topics.append(k)
 
     return new_know_dataset, new_labels, new_topics
 
